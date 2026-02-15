b8e1a51fbaa3237200f1974588ece0e2304d7e41
tirlochan016 <trilochan@crashnova.com>
2026-02-15 18:46:06 +0530

    feat: add CI pipeline

---
+++ index.js
@@ -0,0 +1,2 @@
+console.log('hello devops');
+console.log('i am learning ci/cd using github actions');
---
+++ test.js
@@ -0,0 +1,5 @@
+console.log('starting test')
+setTimeout(() => {
+    console.log('after 3 seconds')
+}, 2=3000)
+console.log('test completed')
